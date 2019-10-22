8f8097c0992c431cceba07d4a6ef15a77a4be4ba
reiseij <rasmussavolainen@hotmail.com>
2019-10-22 10:23:40 +0300

    Projektien ja komponenttien luonti

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angkotisivu';
+  title = 'Rasmuksen Angular -kotisivu';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MeComponent } from './me/me.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { StudiesComponent } from './studies/studies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MeComponent,
+    HobbiesComponent,
+    StudiesComponent
   ],
   imports: [
     BrowserModule,
